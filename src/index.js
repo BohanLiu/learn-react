07a8dae0200244bd18a3b9fa549447c6799571ee
Bohan Liu <lbh.8.26@gmail.com>
2018-03-08 15:11:10 +1100

    taking turns

--- src/index.js
+++ src/index.js
@@ -37,6 +37,7 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
   /** 
@@ -47,10 +48,12 @@ class Board extends React.Component {
   */
 
   handleClick(i) {
-    // alert(i);
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -64,7 +67,8 @@ class Board extends React.Component {
 
   render() {
     console.log('render board');
-    const status = 'Next player: X';
+
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
